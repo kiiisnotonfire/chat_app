16e1175cdd5dd6d1b35c8ca9826d853e453cb879
Kiichi Imai <masamune.kiibook47@gmail.com>
2017-08-08 23:41:06 +0900

    [add] 1対1でのチャット機能追加

---
+++ app/jobs/message_broadcast_job.rb
@@ -0,0 +1,11 @@
+class MessageBroadcastJob < ApplicationJob
+  queue_as :default
+
+  def perform(message)
+    ActionCable.server.broadcast 'room_cahnnel', message: render_message(message)
+  end
+
+  private
+   def render_message(message)
+     ApplicationController.render.render(partial: 'messages/message', locals: { message: message })
+   end
