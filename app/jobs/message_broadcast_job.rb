6453ebbef04b82c01100afbf8328a58a661feefd
Kiichi Imai <masamune.kiibook47@gmail.com>
2017-08-09 13:40:03 +0900

    fix Syntxerror message_broad_cast.rb

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -1,11 +1,12 @@
 class MessageBroadcastJob < ApplicationJob
   queue_as :default
-
+  
   def perform(message)
-    ActionCable.server.broadcast 'room_cahnnel', message: render_message(message)
+    ActionCable.server.broadcast 'room_channel', message: render_message(message)
   end
 
   private
-   def render_message(message)
-     ApplicationController.render.render(partial: 'messages/message', locals: { message: message })
-   end
+    def render_message(message)
+      ApplicationController.renderer.render(partial: 'messages/message', locals: { message: message })
+    end
+end
